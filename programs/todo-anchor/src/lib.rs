bc23dd66c02eb052591f4ec07720c9ed6d9077c8
Nam Vu <namvu@nams-air.lan>
2023-01-13 11:19:01 +0700

    fet: init profile & add todo

--- programs/todo-anchor/src/lib.rs
+++ programs/todo-anchor/src/lib.rs
@@ -1,15 +1,195 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("43SbRAGwoUAsHztWcsDKphHVHSk2fQ3BTUtVYkMKgzbu");
+
+#[constant]
+pub const USER_TAG: &[u8] = b"USER_STATE";
+
+#[constant]
+pub const TODO_TAG: &[u8] = b"TODO_STATE";
+
+#[error_code]
+pub enum TodoError {
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+    #[msg("Not allowed")]
+    NotAllowed,
+    #[msg("Math operation overflow")]
+    MathOverflow,
+    #[msg("Already marked")]
+    AlreadyMarked,
+}
 
 #[program]
 pub mod todo_anchor {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_user(_ctx: Context<InitializeUser>) -> Result<()> {
+        // Initialize user profile with default data
+        let user_profile = &mut _ctx.accounts.user_profile;
+        user_profile.authority = _ctx.accounts.authority.key();
+        user_profile.last_todo = 0;
+        user_profile.todo_count = 0;
+
         Ok(())
     }
+
+    pub fn add_todo(_ctx: Context<AddTodo>, _content: String) -> Result<()> {
+        let todo_account = &mut _ctx.accounts.todo_account;
+        let user_profile = &mut _ctx.accounts.user_profile;
+
+        // Fill contents with argument
+        todo_account.authority = _ctx.accounts.authority.key();
+        todo_account.idx = user_profile.last_todo;
+        todo_account.content = _content;
+        todo_account.marked = false;
+
+        // Increase todo idx for PDA
+        user_profile.last_todo = user_profile.last_todo
+            .checked_add(1)
+            .unwrap();
+
+        // Increase total todo count
+        user_profile.todo_count = user_profile.todo_count
+            .checked_add(1)
+            .unwrap();
+
+        Ok(())
+    }
+
+    pub fn mark_todo(_ctx: Context<MarkTodo>, _todo_idx: u8) -> Result<()> {
+        let todo_account = &mut _ctx.accounts.todo_account;
+        require!(!todo_account.marked, TodoError::AlreadyMarked);
+
+        // Mark todo
+        todo_account.marked = true;
+        Ok(())
+    }
+
+    pub fn remove_todo(ctx: Context<RemoveTodo>, _todo_idx: u8) -> Result<()> {
+        // Decreate total todo count
+        let user_profile = &mut ctx.accounts.user_profile;
+        user_profile.todo_count = user_profile.todo_count
+            .checked_sub(1)
+            .unwrap();
+        Ok(())
+    }
+
+}
+
+#[account]
+#[derive(Default)]
+pub struct UserProfile {
+    pub authority: Pubkey,
+    pub last_todo: u8,
+    pub todo_count: u8
+}
+
+#[account]
+#[derive(Default)]
+pub struct TodoAccount {
+    pub authority: Pubkey,
+    pub idx: u8,
+    pub content: String,
+    pub marked: bool,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction()]
+pub struct InitializeUser<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(
+        init,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = 8 + std::mem::size_of::<UserProfile>(),
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction()]
+pub struct AddTodo<'info> {
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        init,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[user_profile.last_todo].as_ref()],
+        bump,
+        payer = authority,
+        space = std::mem::size_of::<TodoAccount>() + 8,
+    )]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(todo_idx: u8)]
+pub struct MarkTodo<'info> {
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        mut,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[todo_idx].as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(todo_idx: u8)]
+pub struct RemoveTodo<'info> {
+    #[account(
+        mut,
+        seeds = [USER_TAG, authority.key().as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub user_profile: Box<Account<'info, UserProfile>>,
+
+    #[account(
+        mut,
+        close = authority,
+        seeds = [TODO_TAG, authority.key().as_ref(), &[todo_idx].as_ref()],
+        bump,
+        has_one = authority,
+    )]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn bump(seeds: &[&[u8]], program_id: &Pubkey) -> u8 {
+    let (_found_key, bump) = Pubkey::find_program_address(seeds, program_id);
+    bump
+}
--- tests/todo-anchor.ts
+++ tests/todo-anchor.ts
@@ -1,16 +1,68 @@
 import * as anchor from "@project-serum/anchor";
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+
+import {
+  Connection,
+  Keypair,
+  PublicKey
+} from '@solana/web3.js';
+
 import { Program } from "@project-serum/anchor";
 import { TodoAnchor } from "../target/types/todo_anchor";
+import { BN } from "bn.js";
 
 describe("todo-anchor", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
+ 
   const program = anchor.workspace.TodoAnchor as Program<TodoAnchor>;
+  
+  let secretKey = new Uint8Array([186,156,43,50,125,172,233,34,11,236,250,253,34,27,74,253,92,205,215,153,209,124,179,142,217,23,225,171,165,202,90,130,21,249,30,42,81,163,56,63,5,234,10,3,81,17,229,35,98,101,153,103,86,154,204,7,215,254,92,26,156,209,155,32]);
+  let defaultAccount = Keypair.fromSecretKey(secretKey);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Initalising profile!", async () => {
+      
+    const [userProfilePDA] = await anchor.web3.PublicKey.findProgramAddress([
+      utf8.encode('USER_STATE'),
+      defaultAccount.publicKey.toBuffer()
+    ], program.programId)
+  
+    // const tx = await program.methods.initializeUser().accounts({
+    //   authority: defaultAccount.publicKey,
+    //   userProfile: userProfilePDA,
+    //   systemProgram: anchor.web3.SystemProgram.programId,
+    // })
+    // .signers([defaultAccount])
+    // .rpc();
+    // console.log("Your transaction signature", tx);
+  
+   
+  });
+
+  it("Add todo!", async () => {    
+    const [userProfilePDA] = await anchor.web3.PublicKey.findProgramAddress([
+      utf8.encode('USER_STATE'),
+      defaultAccount.publicKey.toBuffer()
+    ], program.programId)
+
+    const userProfileData = await program.account.userProfile.fetch(userProfilePDA);
+
+    console.log(userProfileData)
+
+    const [todoAccountPDA, todoNonce] = await anchor.web3.PublicKey.findProgramAddress([
+      utf8.encode('TODO_STATE'),
+      defaultAccount.publicKey.toBuffer(),
+      Buffer.from([userProfileData.lastTodo])
+    ], program.programId)
+
+    const tx = await program.methods.addTodo("new todo").accounts({
+      userProfile: userProfilePDA,
+      todoAccount: todoAccountPDA,
+      authority: defaultAccount.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([defaultAccount])
+    .rpc();
     console.log("Your transaction signature", tx);
   });
 });
